5b846f87ccb77ae9c12e64138bfb5cc5d315b880
jemian <prjemian@gmail.com>
2015-05-31 18:02:49 -0500

    fixes #2

--- README.rst
+++ README.rst
@@ -14,3 +14,112 @@ For sure, we need more documentation here.
    annotating the contents of each directory.
    Perhaps, also, having a top-level directory
    of really GOOD examples.
+
+NeXus compliance of files
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+An automated analysis of the files, by directory, 
+in this <exampledata> repository is shown below.
+Only HDF5 files that satisfy the structure::
+
+            <file_root>:
+                entry (NXentry)
+                    data (NXdata)
+                        <dataset>:
+                            @signal = 1
+
+are identified as *NeXus HDF5* files at this time.
+
+
+.. --- CRITIQUE report starts after this line ---
+.. date: 2015-05-31
+
+
+<exampledata>
++++++++++++++
+:.project:  not HDF5 file
+:.pydevproject:  not HDF5 file
+:MANIFEST.in:  not HDF5 file
+:README.rst:  not HDF5 file
+:critique.py:  not HDF5 file
+:simple3D.h5:  NeXus HDF5 file, 1 **NXentry** group
+:verysimple.xml:  not HDF5 file
+
+<exampledata>/ANSTO
++++++++++++++++++++
+:PLP0006018.nx.hdf:  NeXus HDF5 file, 1 **NXentry** group
+
+<exampledata>/APS/CCDImageServer
+++++++++++++++++++++++++++++++++
+:README.txt:  not HDF5 file
+:Smither400.3_apr0300051.hdf:  not HDF5 file
+:Smither400.3_apr0300052.hdf:  not HDF5 file
+:Smither400.3_apr0300053.hdf:  not HDF5 file
+:Smither400.3_apr0300054.hdf:  not HDF5 file
+:Smither400.3_apr0300055.hdf:  not HDF5 file
+:recon_0123.hdf:  not HDF5 file
+:recon_0124.hdf:  not HDF5 file
+:recon_0125.hdf:  not HDF5 file
+
+<exampledata>/APS/EPICSareaDetector/NeXus-plugin
+++++++++++++++++++++++++++++++++++++++++++++++++
+:AgBehenate_228.hdf5:  NeXus HDF5 file, 1 **NXentry** group
+
+<exampledata>/APS/other
++++++++++++++++++++++++
+:ID34_not_complete.h5:  NeXus HDF5 file, 1 **NXentry** group
+
+<exampledata>/APS/scan2nexus
+++++++++++++++++++++++++++++
+:14BMC_0015.mda:  not HDF5 file
+:14BMC_0015.nexus:  not HDF5 file
+:14BMC_0015.xml:  not HDF5 file
+:2iddf_0106.mda:  not HDF5 file
+:2iddf_0106.nexus:  not HDF5 file
+:2iddf_0106.xml:  not HDF5 file
+:README:  not HDF5 file
+:mts_0347.mda:  not HDF5 file
+:mts_0347.nexus:  not HDF5 file
+:mts_0347.xml:  not HDF5 file
+:mts_0348.mda:  not HDF5 file
+:mts_0348.nexus:  not HDF5 file
+:mts_0348.xml:  not HDF5 file
+:sample.mda.text:  not HDF5 file
+:sample1.mda:  not HDF5 file
+:sample1.nexus:  not HDF5 file
+:sample1.xml:  not HDF5 file
+
+<exampledata>/APS/tomo
+++++++++++++++++++++++
+:Tomography_metadata.hdf:  not HDF5 file
+:Tomography_raw.hdf:  not HDF5 file
+
+<exampledata>/IPNS/LRMECS
++++++++++++++++++++++++++
+:lrcs3701.nx5:  NeXus HDF5 file, 2 **NXentry** groups
+:lrcs3701.nxs:  not HDF5 file
+
+<exampledata>/Soleil
+++++++++++++++++++++
+:file_1.nxs:  NeXus HDF5 file, 1 **NXentry** group
+:file_2.nxs:  NeXus HDF5 file, 1 **NXentry** group
+
+<exampledata>/code/hdf4
++++++++++++++++++++++++
+:NXtest.hdf:  not HDF5 file
+:dmc01.hdf:  not HDF5 file
+:dmc02.hdf:  not HDF5 file
+
+<exampledata>/code/hdf5
++++++++++++++++++++++++
+:NXtest.h5:  HDF5 file, 2 **NXentry** groups
+:dmc01.h5:  NeXus HDF5 file, 1 **NXentry** group
+:dmc02.h5:  NeXus HDF5 file, 1 **NXentry** group
+:focus2007n001335.hdf:  NeXus HDF5 file, 1 **NXentry** group
+:sans2009n012333.hdf:  NeXus HDF5 file, 1 **NXentry** group
+
+<exampledata>/code/xml
+++++++++++++++++++++++
+:NXtest.xml.txt:  not HDF5 file
+:dmc01.xml.txt:  not HDF5 file
+:dmc02.xml.txt:  not HDF5 file
---
+++ critique.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env Python
+
+'''describe NeXus compliance of files in this repository'''
+
+
+import os
+import sys
+import h5py
+import spec2nexus.h5toText
+
+
+class Critic(object):
+    '''
+    describe a file in terms of NeXus compliance
+    
+    :param str fname: (absolute or relative path and) name of file
+    '''
+    
+    def __init__(self, path, fname):
+        self.path = None
+        self.fname = None
+        self.hdf5 = None
+        self.NXentry_nodes = []
+        self.isNeXus = False
+
+        # can the file be found?
+        fullname = os.path.join(path, fname)
+        if not os.path.exists(fullname):
+            return
+
+        # Is it really a file?
+        if not os.path.isfile(fullname):
+            return
+        
+        # ok, passes basic qualifications, proceed
+        self.path = path
+        self.fname = fname
+        
+        # alternative to find_self.NXentry_NXdata_nodes(), deeper analysis
+        # this code only checks for NXentry/NXdata/<data>/@signal=1
+        self.isNeXus = spec2nexus.h5toText.isNeXusFile(fullname)
+
+        # is it an HDF5 file?
+        if not self.openHDF5(fullname):
+            return
+
+        self.NXentry_nodes = self.find_NX_class_nodes(self.hdf5)
+        for entry in self.NXentry_nodes:
+            self.find_NXentry_NXdata_nodes(entry)
+
+        # TODO: make lists of
+        #    all field names
+        #    group names
+        #    nx_class types
+        # TODO: check all field names
+        #    field names for fit to the regexp
+        #    field names for correct codepage
+        #    strings for correct codepage
+
+        # finally: close the HDF5 file
+        self.hdf5.close()
+    
+    def describe_file(self):
+        s = 'HDF5 file'
+        if self.hdf5 is None:
+            return 'not ' + s
+        if self.isNeXus:
+            s = 'NeXus ' + s
+        if len(self.NXentry_nodes) > 0:
+            s += ', %d **NXentry** group' % len(self.NXentry_nodes)
+            if len(self.NXentry_nodes) > 1:
+                s += 's'
+        return s
+
+    def __str__(self, *args, **kwargs):
+        return self.describe_file()
+    
+    def openHDF5(self, fname):
+        '''try to open the file as HDF5'''
+        try:
+            self.hdf5 = h5py.File(fname, 'r')
+        except IOError:
+            return False
+        return True    
+    
+    def find_NX_class_nodes(self, parent = None, nx_class = 'NXentry'):
+        '''identify the NXentry (or as specified) nodes'''
+        parent = parent or self.hdf5
+        node_list = []
+        for node in parent.values():
+            if spec2nexus.h5toText.isNeXusGroup(node, nx_class):
+                node_list.append(node)
+        return node_list   
+    
+    def find_NXentry_NXdata_nodes(self, entry):
+        '''
+        identify if file satisfies valid NeXus NXentry/NXdata structure
+        
+        Either this structure::
+        
+            <file_root>:
+                entry (NXentry)
+                    data (NXdata)
+                        <dataset>:
+                            @signal = 1
+        
+        ..  search for "NXdata" in:
+            http://wiki.nexusformat.org/NIAC2014_Meeting#Minutes
+            http://wiki.nexusformat.org/2014_axes_and_uncertainties
+        
+        or this structure (valid starting 2015)::
+        
+            <file_root>:
+                @signal = "entry01"      (only needed to resolve ambiguity)
+                entry01 (NXentry)
+                    @signal = "data02"   (only needed to resolve ambiguity)
+                    data01 (NXdata)
+                        ...
+                    data02 (NXdata)
+                        @signal = "data1"
+                        data1
+                        data2
+                entry02 (NXentry)
+                    ...
+        
+        :see: http://wiki.nexusformat.org/2014_How_to_find_default_data
+        '''
+        # TODO: under construction
+        NXdata_nodes = self.find_NX_class_nodes(entry, nx_class = 'NXdata')
+        for node in NXdata_nodes:
+            # print node
+            pass
+
+
+class Registrar(object):
+    '''keep track of critiqued files in an internal dictionary'''
+
+    def __init__(self):
+        self.db = {}
+
+    def add(self, path, critic):
+        '''add new critique to the database'''
+        if critic.fname is None:
+            return
+        if path not in self.db:
+            self.db[path] = {}
+        self.db[path][critic.fname] = critic
+    
+    def report(self):
+        for path, flist in sorted(self.db.items()):
+            print '\n' + path + '\n' + '+'*len(path)
+            for fname, critique in sorted(flist.items()):
+                print ':'+fname+': ', str(critique)
+
+
+def walk_function(registrar, path, files):
+    '''
+    called for each directory traversed
+    
+    :param obj registrar: instance of Registrar(), database of analyzed files
+    :param str path: subdirectory name
+    :param [str] files: list of files in *path* directory
+    '''
+    if path.find('.git') > -1:      # skip the Git VCS directory
+        return
+    for nm in files:
+        registrar.add(path, Critic(path, nm))
+
+
+def main(path = None):
+    '''traverse a directory and describe how each file conforms to NeXus'''
+    registrar = Registrar()
+    path = path or os.path.dirname(__file__)
+    os.path.walk(path, walk_function, registrar)
+    registrar.report()
+    # TODO: should modify the README.rst
+    #    after the line that reads:
+    #    .. --- CRITIQUE report starts after this line ---
+
+
+if __name__ == '__main__':
+    main()
